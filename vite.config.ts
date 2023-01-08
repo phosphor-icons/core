56c8d5f2998e60844390ead0e825b993db9b7437
rektdeckard <friedtm@gmail.com>
2023-01-08 01:28:37 -0700

    v1.4.3

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "Phosphor",
-      fileName: "phosphor-core",
+      fileName: "index",
     },
   },
 })
