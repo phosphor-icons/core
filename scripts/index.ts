f62dad6c5b274d790aa70bf7b71636f3dcfa83d5
rektdeckard <friedtm@gmail.com>
2024-01-31 19:37:34 -0700

    build(scripts): add more assertions for metadata and assets

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,20 @@
 "use strict";
 
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { version } from "../package.json";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const ASSETS_PATH = path.join(__dirname, "../assets");
+export const RAW_PATH = path.join(__dirname, "../raw");
+export const CATALOG_PATH = path.join(__dirname, "../src/icons.ts");
+
+const [MAJOR_PART, MINOR_PART] = version.split(".");
+export const CURRENT_NUMERIC_VERSION = +MAJOR_PART + +MINOR_PART / 10;
+
 export const CATEGORY_MAP = {
   Arrows: "ARROWS",
   Brands: "BRAND",
@@ -19,4 +34,33 @@ export const CATEGORY_MAP = {
   "System & Devices": "SYSTEM",
   Time: "TIME",
   "Weather & Nature": "WEATHER",
+};
+
+export type IconStatus =
+  | "Backlog"
+  | "Designing"
+  | "Designed"
+  | "Implemented"
+  | "Deprecated"
+  | "";
+
+export type IconMetadata = {
+  name: string;
+  alias?: string;
+  rid: string;
+  category: keyof typeof CATEGORY_MAP;
+  tags: string[];
+  search_categories: string[];
+  status: IconStatus;
+  codepoint?: number;
+  published: boolean;
+  published_in: number;
+  updated_in: number;
+  notes?: string;
+};
+
+export type IconAPIResponse = {
+  icons: IconMetadata[];
+  count: number;
+  version: number;
 };
